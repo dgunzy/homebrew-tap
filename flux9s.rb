a9f2d13c4c4e17341d6092365abff6426e28aaa0
Daniel Guns <danbguns@gmail.com>
2025-11-11 21:28:13 -0400

    v0.1.2 -tweak
    Signed-off-by: Daniel Guns <danbguns@gmail.com>

--- flux9s.rb
+++ flux9s.rb
@@ -6,17 +6,17 @@ class Flux9s < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/dgunzy/flux9s/releases/download/v0.1.0/flux9s-macos-x86_64.tar.gz"
+      url "https://github.com/dgunzy/flux9s/releases/download/v0.1.2/flux9s-macos-x86_64.tar.gz"
       sha256 "4a369aeab54b8d91a6f7caa9336fe7c21a01abc5ba541caf9976c8de69da1a51"
     elsif Hardware::CPU.arm?
-      url "https://github.com/dgunzy/flux9s/releases/download/v0.1.0/flux9s-macos-aarch64.tar.gz"
+      url "https://github.com/dgunzy/flux9s/releases/download/v0.1.2/flux9s-macos-aarch64.tar.gz"
       sha256 "621b3e5b2f1a161808327c9f033304c9d4d168629103007aebf6ea17c4ddb79a"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/dgunzy/flux9s/releases/download/v0.1.0/flux9s-linux-x86_64.tar.gz"
+      url "https://github.com/dgunzy/flux9s/releases/download/v0.1.2/flux9s-linux-x86_64.tar.gz"
       sha256 "ebb1160a0cdee4ad2531dc10430bbbad9c27200298842ef045006ab2752b15d2"
     end
   end
