328b658aa895b23d73e50b6099d542f1e07ccccf
Daniel Guns <danbguns@gmail.com>
2025-11-11 21:27:39 -0400

    v0.1.2
    Signed-off-by: Daniel Guns <danbguns@gmail.com>

--- flux9s.rb
+++ flux9s.rb
@@ -7,14 +7,17 @@ class Flux9s < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/dgunzy/flux9s/releases/download/v0.1.0/flux9s-macos-x86_64.tar.gz"
-      sha256 "5d80960de560b6db187c848fbf77b16eea5fc2cb791c94de67291c620d0c111b"
+      sha256 "4a369aeab54b8d91a6f7caa9336fe7c21a01abc5ba541caf9976c8de69da1a51"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/dgunzy/flux9s/releases/download/v0.1.0/flux9s-macos-aarch64.tar.gz"
+      sha256 "621b3e5b2f1a161808327c9f033304c9d4d168629103007aebf6ea17c4ddb79a"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/dgunzy/flux9s/releases/download/v0.1.0/flux9s-linux-x86_64.tar.gz"
-      sha256 "454909d2bdafe6b69cbb231b88173c4423e307640b94a3230dd1d23bddbd9493"
+      sha256 "ebb1160a0cdee4ad2531dc10430bbbad9c27200298842ef045006ab2752b15d2"
     end
   end
 
