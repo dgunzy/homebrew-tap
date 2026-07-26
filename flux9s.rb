8531748b6703cba0f4f7e5f36861153583e35c7d
Daniel Guns <danbguns@gmail.com>
2026-07-26 10:38:53 -0300

    bugfix bad tap
    Signed-off-by: Daniel Guns <danbguns@gmail.com>

--- flux9s.rb
+++ flux9s.rb
@@ -1,22 +1,22 @@
 class Flux9s < Formula
   desc "K9s-inspired terminal UI for monitoring Flux GitOps resources"
   homepage "https://github.com/dgunzy/flux9s"
-  version "refs/heads/main"
+  version "1.0.0"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/dgunzy/flux9s/releases/download/vrefs/heads/main/flux9s-macos-x86_64.tar.gz"
+      url "https://github.com/dgunzy/flux9s/releases/download/v1.0.0/flux9s-macos-x86_64.tar.gz"
       sha256 "ba68a59bd404117bbc3fae8407a5e38595d57090620aa799e1c3a79444bc68ad"
     elsif Hardware::CPU.arm?
-      url "https://github.com/dgunzy/flux9s/releases/download/vrefs/heads/main/flux9s-macos-aarch64.tar.gz"
+      url "https://github.com/dgunzy/flux9s/releases/download/v1.0.0/flux9s-macos-aarch64.tar.gz"
       sha256 "305a919a77bfca31ebb6b5aee0c001bff6f9e6dc68b61a61ab6b3e8108d9c908"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/dgunzy/flux9s/releases/download/vrefs/heads/main/flux9s-linux-x86_64-musl.tar.gz"
+      url "https://github.com/dgunzy/flux9s/releases/download/v1.0.0/flux9s-linux-x86_64-musl.tar.gz"
       sha256 "b810b72d697c4c4798027eaabde0e9c8862c508cab3970f5a06a47f47a0e79d9"
     end
   end
@@ -26,6 +26,6 @@ class Flux9s < Formula
   end
 
   test do
-    system "#{bin}/flux9s", "--version"
+    system "#{bin}/flux9s", "version"
   end
 end
